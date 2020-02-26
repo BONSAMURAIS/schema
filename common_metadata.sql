5e2338ef26cc076f2a6287d80660afeb639f3871
Chris Mutel <cmutel@gmail.com>
2020-02-26 22:12:49 +0100

    Add stub for common metadata

---
+++ common_metadata.sql
@@ -0,0 +1,7 @@
+-- Common metadata for the BONSAI project: https://bonsai.uno/
+-- Assumes schema from https://github.com/BONSAMURAIS/schema/
+-- BONSAI mailing list: https://bonsai.groups.io/g/main/
+
+-- After initial release, please make all changes following the pull request
+-- workflow to allow for community review. Large changes will probably require
+-- a BEP: https://github.com/BONSAMURAIS/enhancements
